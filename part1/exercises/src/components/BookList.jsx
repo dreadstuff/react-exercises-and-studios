c971d10df3db92e8d13e15fa0bde782a76135325
dreadstuff <gavinkolasa@gmail.com>
2024-08-18 21:04:06 -0500

    commit part 1 exercises

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "List of AWESOME book series!";
+   let book1 = "The Foundation";
+   let book2 = "Red Rising";
+   let book3 = "StarWars Series";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src="https://m.media-amazon.com/images/I/81J4LjdqQFL._AC_UF1000,1000_QL80_.jpg" width="200" alt="The Foundation - Book" />
+         <img src="https://images.booksense.com/images/786/539/9780345539786.jpg" width="210" alt="Red Rising - Book" /><br></br>
+         <img src="https://m.media-amazon.com/images/I/91P3-Xdp6kL._AC_UF1000,1000_QL80_.jpg" width="300" alt="StarWars Series - Collection" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,14 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return <p>
+      <div className='choresText'>
+      <ol> <h2 className={classes.choresHeading}>Chores to do!</h2>
+         <li className={classes.choresText}>Finish Laundry</li>
+         <li className={classes.choresText}>Fold Clothes</li>
+         <li className={classes.choresText}>Empty Dishwasher</li>
+      </ol>
+      </div>   
+   </p>;
 }
+
