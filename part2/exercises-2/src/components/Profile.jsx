bcd78cf5131d2b595fcd8dfc895f2f47e5bcb852
Gavin Kolasa <dreadstuff@gmail.com>
2024-08-24 15:51:00 -0500

    commit for ex 1

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,30 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
+const listItem = oceans.map(oceans =>
+      <div key={oceans.id}  className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`}>
+         <img src={oceans.image} alt={oceans.name} className='img' />
+         <h1>{oceans.name}</h1>
+         <h2>Cool facts!</h2>
+         <ol>
+            <li>{oceans.fact1}</li>
+            <li>{oceans.fact2}</li>
+            <li>{oceans.fact3}</li>
+         </ol>
+         <Button />
+      </div>
+   );
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
+         <ul>
+            {listItem}
+         </ul>
       </>  
-   );
-}
+)};
 
 export default Profile;
+
